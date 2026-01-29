ff57c75092e01da30b946a2084dc016547288f28
𝐇𝐚𝐫𝐮𝐱𝐚 <Fahryadityagithub@gmail.com>
2026-01-29 15:24:38 +0800

    Add Main class with Hello World example

---
+++ BelajarJava.java
@@ -0,0 +1,13 @@
+// Mendefinisikan class bernama Main
+// Nama class HARUS sama dengan nama file (Main.java)
+public class Main {
+
+    // Method utama (main method)
+    // Program Java selalu mulai dieksekusi dari sini
+    public static void main(String[] args) {
+
+        // Mencetak teks "Hello World" ke layar
+        System.out.println("Hello World");
+
+    }
+}
